3be8f1a5cd650a4becbac55f076ffd00691dc73e
Your Name <dagkotecka@gmail.com>
2017-01-20 02:32:06 +0100

    few changes

--- Client/app/src/main/java/com/mkoi/client/MainActivity.java
+++ Client/app/src/main/java/com/mkoi/client/MainActivity.java
@@ -1,13 +1,120 @@
 package com.mkoi.client;
 
+import android.content.Context;
+import android.graphics.Path;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.URL;
+import java.net.UnknownHostException;
+import java.security.MessageDigest;
+import java.util.Arrays;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button sendFileBtn, loadFileBtn, listServerBtn;
+    ListView clientListView, serverListView;
+    private ObjectInputStream sInput; // to read from the socket
+    private ObjectOutputStream sOutput; // to write on the socket
+    public Socket socket;
+    String clientPrivateKey;
+    String serverPrivateKey;
+    String encryptedString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sendFileBtn = (Button)findViewById(R.id.sendFileBtn);
+        loadFileBtn = (Button)findViewById(R.id.loadFileBtn);
+        listServerBtn = (Button)findViewById(R.id.listServerFilesBtn);
+        clientListView = (ListView)findViewById(R.id.clientFilesListView);
+        serverListView = (ListView)findViewById(R.id.serverFilesListView);
+        try {
+            clientPrivateKey = getClientKey("client.key");
+            serverPrivateKey = getClientKey("server.key");
+            byte[] encrypted = encrypt(serverPrivateKey.getBytes(), clientPrivateKey.getBytes());
+            //encryptedString = new String(encrypted, "UTF-8");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        connect();
+
+
+
+    }
+    public void connect() {
+        Runnable test = new Runnable() {
+
+            public void run() {
+
+                InetAddress serverAddr = null;
+                try {
+                    serverAddr = InetAddress.getByName("10.0.2.2");
+                } catch (UnknownHostException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                Log.i("ClientActivity", "C: Connecting...");
+                try {
+                    socket = new Socket("10.0.2.2", 8888);
+                    DataOutputStream dOut = new DataOutputStream(socket.getOutputStream());
+
+                    // Send first message
+                    dOut.writeByte(1);
+                    dOut.writeUTF("This is the first type of message.");
+                    dOut.flush(); // Send off the data                }
+
+                }
+                catch(Exception e){
+                }
+            }
+
+            };
+
+        new Thread(test).start();
+    }
+    public String getClientKey(String fileName) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(getAssets().open(fileName)));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line).append("\n");
+        }
+        reader.close();
+        return sb.toString();
+    }
+
+    private static byte[] encrypt(byte[] raw, byte[] key) throws Exception {
+        MessageDigest sha = MessageDigest.getInstance("SHA-1");
+        key = sha.digest(key);
+        key = Arrays.copyOf(key, 16);
+        SecretKeySpec skeySpec = new SecretKeySpec(key, "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
+        byte[] encrypted = cipher.doFinal(raw);
+        return encrypted;
     }
 }
+
+
